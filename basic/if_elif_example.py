dd49d9f2b9cf360693adee58b6bca56d43e2e21c
amiraj nigam <amirajnigam@gmail.com>
2021-09-17 01:47:16 -0700

    if, else, elif statements

---
+++ basic/if_elif_example.py
@@ -0,0 +1,10 @@
+name =  'Bob'
+age = 3000
+if name == 'Alice':
+    print('Hi Alice')
+elif age < 12:
+    print('You are not alice kido')
+elif age > 2000:
+    print('Unlike you, Alice is not an undead, immortal vampire')
+elif age > 100:
+    print(' you are not Alice, grannie.')
