4ebfc17acf45f3aabb8f5d85bb04752f2e52bc40
amiraj nigam <amirajnigam@gmail.com>
2021-11-08 09:57:19 -0800

    Enumerate

---
+++ Advance_Concepts/Enumeration_function/tut11.py
@@ -0,0 +1,15 @@
+a = ['CodewithHarry', 'T Series', 'Mixer Grinder', 'Pen']
+
+#Task: print the channels which are at index 2
+
+# i = 0
+# for item in a:
+#     i= i+1
+#     if i%2 == 0:
+#         print(item)
+
+#Using Enumerate to perform the above task
+
+for i, item in enumerate(a):
+    if(i+1)%2 == 0:
+        print(item)
