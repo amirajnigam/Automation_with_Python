666d483072d93df6b4c4d8aaac6d4319485f8fb0
amiraj nigam <amirajnigam@gmail.com>
2021-11-10 09:28:15 -0800

    for loop

---
+++ Basic_Concepts/for_loop/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+print('My name is')
+# for i in range(0, 10, 2):                                                      
+#     print('Jimmy five times ' + str(i))
---
+++ Basic_Concepts/for_loop/tut2.py
@@ -0,0 +1,45 @@
+# print('My name is')
+# for i in range(5):                                                      #In for loop i is not initialized to zero because of the range function it takes vales of range function and range function always starts from zero
+#     print('Jimmy five times ' + str(i))
+
+#equivalent while loop
+# print('My name is')
+# i = 0                                                                   #with for loop we do not have to write i = 0 and i = i + 1 extra steps
+# while i < 5:                                                      
+#     print('Jimmy five times ' + str(i))
+#     i = i + 1
+
+
+########################################################################################
+
+#Print sum of first 100 numbers
+# total = 0
+# for num in range(101):
+#     total = total + num
+# print(total)
+
+########################################################################################
+
+#range function example with for loop
+# print('My name is')
+# for i in range(12, 16):                                                      
+#     print('Jimmy five times ' + str(i))
+
+
+#range with 3 arguments(start, stop, step)
+# print('My name is')
+# for i in range(0, 10, 2):                                                      
+#     print('Jimmy five times ' + str(i))
+
+#range wioth negative number
+print('My name is')
+for i in range(5, -1, -1):                                                      
+    print('Jimmy five times ' + str(i))
+
+
+""""
+* A "for" loop will loop a specific number of times.
+* The range() function can be called with one, two, or three arguments.
+* The break and continue statements can be used in for loops just like they're used in while loops.
+
+"""
